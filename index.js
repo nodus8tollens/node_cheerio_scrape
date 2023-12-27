92574d40587edb0c994df7c80703326c11a195aa
nodus8tollens <nodus8tollens>
2023-12-27 12:31:48 +0100

    Cleaned. Changed async file system functionts to sync

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if (!fs.existsSync(outputDirectory)) {
 
 // URL of the website you want to scrape
 const url =
-  "https://www.marketsandmarkets.com/Market-Reports/contact-center-software-market-257044641.html";
+  "https://www.marketsandmarkets.com/Market-Reports/concrete-admixtures-additives-market-768.html";
 //Regex for extracting filename
 const regex = /Reports\/(.*?)\-market/;
 const match = url.match(regex);
@@ -31,34 +31,32 @@ axios
     // Load the HTML content into Cheerio
     const $ = cheerio.load(response.data);
 
-    //First Paragraph Content
-    const marketValueParagraph = $(".reportTabContent")
-      .children()
-      .first()
-      .text()
-      .trim(); //Good
+    //marketValueParagraph is the first <p> child of the container with ".reportTabContent" class
+    const marketValueParagraph = $(".reportTabContent").children().first();
 
-    //Create output file and write first paragraph (add title/keyword for paragraph)
+    //Create output file and write marketValueParagraph (add title/keyword for paragraph)
     const filePath = outputDirectory + `/${filename}.txt`;
-    fs.writeFile(filePath, "MARKET VALUE: \n" + marketValueParagraph, (err) => {
-      if (err) {
-        console.error("Error creating file: ", err);
-      } else {
-        console.log("File created: ", filePath);
+    fs.writeFileSync(
+      filePath,
+      "SOURCE: Markets and Markets \n\nMARKET VALUE: \n" +
+        marketValueParagraph.text().trim(),
+      (err) => {
+        if (err) {
+          console.error("Error creating file: ", err);
+        } else {
+          console.log("File created: ", filePath);
+        }
       }
-    });
+    );
 
-    //console.log(firstParagraph);
-
-    // Find the <h4> elements with specific content
+    // Select the <h3> above the "Drivers" <h4> and its adjacent <p>
     const marketDynamicsH3 = $('h3:contains("Market Dynamics")');
-    const restraintH4 = $('h4:contains("Restraint:")');
 
     // Find the <p> elements between the two <h4> elements
     const drivers = marketDynamicsH3.next().next();
     drivers.each((index, element) => {
-      //Append content
-      fs.appendFile(
+      //Append content from <h3> and <p> elements containing "Market Drivers" information and add title/keyword
+      fs.appendFileSync(
         filePath,
         "\nDRIVERS: \n" + $(element).text().trim(),
         (err) => {
@@ -69,15 +67,15 @@ axios
           }
         }
       );
-      //console.log("DRIVERS: ", $(element).text().trim());
-    }); //Good
-
+    });
+    //Select the <h3> before the "Segments" section and the <h3> before the "Key Market Players" section
     const ecosystemH3 = $('h3:contains("Ecosystem")');
-    const playersH3 = $('h3:contains("Players:")');
+    const playersH3 = $('h3:contains("Players")');
+    //Extract all HTML between <h3> and <h4>
     const segments = ecosystemH3.nextUntil(playersH3, "h4");
     segments.each((index, element) => {
-      //Append content
-      fs.appendFile(
+      //Append content from "Market Segment" section and add title/keyword
+      fs.appendFileSync(
         filePath,
         "\nSEGMENTS: \n" + $(element).text().trim(),
         (err) => {
@@ -88,12 +86,12 @@ axios
           }
         }
       );
-      //console.log("SEGMENTS: ", $(element).text().trim());
     });
-    //Get the paragraph about key market players
-    const marketPlayersH3 = $('h3:contains("Market Players")');
-    const marketPlayersParagraph = marketPlayersH3.next();
-    fs.appendFile(
+    //Select the "Market Players" <p> that is adjacent to the <h3> containing "Players:"
+    const marketPlayersParagraph = playersH3.next();
+    console.log(marketPlayersParagraph.text().trim());
+    //Append content from "Key Market Players" section and add title/keyword
+    fs.appendFileSync(
       filePath,
       "\nKEY MARKET PLAYERS: \n" + marketPlayersParagraph.text().trim(),
       (err) => {
@@ -104,10 +102,6 @@ axios
         }
       }
     );
-    //console.log("MARKET PLAYERS: ", marketPlayersParagraph.text().trim());
-
-    // Print the extracted titles
-    //console.log("Titles:", titles);
   })
   .catch((error) => {
     console.error("Error fetching the webpage:", error);
